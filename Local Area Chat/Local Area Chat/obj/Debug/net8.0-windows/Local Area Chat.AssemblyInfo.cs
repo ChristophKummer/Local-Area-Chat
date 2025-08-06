ba0d0618558542bd14e396ee26f8f80021aca70a
Christoph Kummer <christophkummer7@gmail.com>
2025-08-06 19:04:05 +0200

    funktionierende Version_bereit zur DBA-Abgabe

--- Local Area Chat/Local Area Chat/obj/Debug/net8.0-windows/Local Area Chat.AssemblyInfo.cs
+++ Local Area Chat/Local Area Chat/obj/Debug/net8.0-windows/Local Area Chat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Local Area Chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5ced51f8832be40d6bf44e0566a560927e630b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ede4f22d214b7fb5e4374e36b9279302502f07c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Local Area Chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Local Area Chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
